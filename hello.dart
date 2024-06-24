7e05be10268710188570e3f979f603c23e1cb736
Prithvi <127526935+Prithvishankarmenon@users.noreply.github.com>
2024-06-24 06:06:58 +0000

    Added Changes

---
+++ hello.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("Hello");
+  
+  String name ="Prithvi";
+  print(name);
+  int num1=6;
+  double  num2=22.1;
+  
+  print("$num1 $num2");
+  
+  List<int> numlist= [10,20,30];
+  print(numlist);
+    
+  print(numlist.isEmpty);
+}
