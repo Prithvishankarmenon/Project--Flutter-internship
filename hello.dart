76c287783480af3f1d13c9daa9cb439b703b76fa
Prithvi <127526935+Prithvishankarmenon@users.noreply.github.com>
2024-06-24 06:46:44 +0000

    Added Changes

--- hello.dart
+++ hello.dart
@@ -1,31 +1,25 @@
-void main(){
+void main() {
   print("Hello");
-  
-  String name ="Prithvi";
+
+  String name = "Prithvi";
   print(name);
-  int num1=6;
-  double  num2=22.1;
-  
+  int num1 = 6;
+  double num2 = 22.1;
+
   print("$num1 $num2");
-  
-  List<int> numlist= [10,20,30];
+
+  List<int> numlist = [10, 20, 30];
   print(numlist);
-    
+
   print(numlist.isEmpty);
-  
-  Map<String,String> ar1 =
-  {
-          "domain":"flutter",
-            "Name":"PS"
+
+  Map<String, String> ar1 = {"domain": "flutter", "Name": "PS"};
+  print(ar1["domain"]);
+  Map<String, dynamic> ar2 = {
+    "domain": ["flutter", 22],
+    "Name": "PS"
   };
-  
-    Map<String,dynamic> ar2 =
-  {
-          "domain":["flutter",22],
-            "Name":"PS"
-  };
+  print(ar2["domain"][1]);
   print(ar2);
   print(ar1);
-
-  
 }
