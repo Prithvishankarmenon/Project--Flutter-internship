3d813d461dee1effffb4f730627739a0984d8de8
Prithvishankarmenon <mr.prithvishankar@gmail.com>
2024-06-25 15:07:16 +0530

    added changes

--- Flutter Project/sample_project/lib/login_screen.dart
+++ Flutter Project/sample_project/lib/login_screen.dart
@@ -7,34 +7,34 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(0,131, 108, 1.0),
-        
+        backgroundColor: Color.fromRGBO(0, 131, 108, 1.0),
         title: const Text("WhatsApp"),
         titleTextStyle: TextStyle(
-          color: Colors.white,
-          fontSize: 25,
-          fontWeight: FontWeight.bold
-        ),
+            color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
         actions: [
           Icon(Icons.search, color: Colors.white60),
           Icon(Icons.more_vert, color: Colors.white60)
         ],
       ),
-
-      body:Column(
-        children: [
-          Text("Hello world",style: TextStyle(
-        fontSize: 20
-      ),),
-      Text("Hello world",style: TextStyle(
-        fontSize: 20
-      ),),
-      Text("Hello world",style: TextStyle(
-        fontSize: 20
-      ),)
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Hello world",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "Hello world",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "Hello world",
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
       ),
-
     );
   }
 }
