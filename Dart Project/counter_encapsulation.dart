d464cd7639c1eed8ad908e18d6ef6e89f0e623c9
Prithvishankarmenon <mr.prithvishankar@gmail.com>
2024-06-25 10:31:10 +0530

    added

---
+++ Dart Project/counter_encapsulation.dart
@@ -0,0 +1,15 @@
+class Counter {
+  int _count =10;
+ 
+  int get count => _count;
+
+  void increment(){
+    _count++;
+  }
+  void decrement(){
+    _count--;
+  }
+  void reset(){
+    _count = 0;
+  }
+}
