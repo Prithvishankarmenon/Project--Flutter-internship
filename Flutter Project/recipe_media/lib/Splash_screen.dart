cd4f8dd16c5687c1de0bbd4f59211666e168227d
Prithvishankarmenon <mr.prithvishankar@gmail.com>
2024-06-30 12:06:28 +0530

    slash screen

--- Flutter Project/recipe_media/lib/Splash_screen.dart
+++ Flutter Project/recipe_media/lib/Splash_screen.dart
@@ -15,37 +15,44 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     navigateToNextScreen();
-   
   }
- 
- void navigateToNextScreen() async{
-  await Future.delayed(Duration(seconds: 3));
-  bool logedIn = await getData();
-    if (logedIn == true) {
-      Navigator.push(
+
+  void navigateToNextScreen() async {
+    await Future.delayed(Duration(seconds: 10));
+    bool loggedIn = await getData();
+    if (loggedIn == true) {
+      Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => HomeScreen()));
-    } else
-      Navigator.push(
+    } else {
+      Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => GetStarted()));
+    }
   }
 
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text(
-          "Welcome",
-          style: TextStyle(
-              color: Colors.orange[900], fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: Column(
+      backgroundColor: Colors.orange[900],
+      body: Stack(
         children: [
-          Image.network(
-              "https://carwow-uk-wp-3.imgix.net/18015-MC20BluInfinito-scaled-e1707920217641.jpg")
+          // Full screen image
+          Positioned.fill(
+            child: Image.network(
+              "https://s3-alpha-sig.figma.com/img/20f8/1fbb/1809c8e760050809e0c3c98d19a20b56?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iZF2POMitinik1CsKYBI8EcGHtxc1~ciZ6VJAckYnGeoPqXR3QG01Sgk5E4gdQgokrZrgxxO-A~KJ8x31TFNNM~cV0TkzZw8vIBIsfop~42woi8Fv~H9ZCG5GEdZyyBZQJunI8BzG2pG40eC1GXd~1h9wYg5FxatCTXoNV8O8aSjDANU4YCb1L1V68bN83lnisXUmLR9llGdX88ukWsC7N6QXnTcQmXOA8L8YxEgN3~gstNZwokqVwaWNcpGOW7XPB3YBwqUAl98ZBE3RKEJkTm2299vjbTIT9Anvol0L6puBe2Au5pCVMUngjWZjs-B7f~zbCwIn56GFf-qv91sHA__",
+              fit: BoxFit.cover,
+            ),
+          ),
+          // Centered image
+          Center(
+            child: Image.network(
+              "https://s3-alpha-sig.figma.com/img/906f/ef6b/74a76c1d0fa6e6b624d6a0dcb586ce03?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dNQVe-PXFBfrxQnt3LYVzLCk1wlRLJ5Bzx3tsXZSTPpEMiVn1HGvbjA5hXdHu0~Cz8EfpU~9EObtTjXTuV-ZAv5OpAJkKjcdIM~ly~qQ2KPDcYt1aQ6ZfrIO3lvBqpiac38o9PpHw2X5BKwQzzHg~WHDOGUMHKMuP4oCsyUuKJQgP0g~UrygbPIso1aNwr4oZTm~afD74hj7PBYlbj4wNL2kQmth4hWtrLtXF~HVOxxpYgSX~tqRkgeNOIqCTl3J3Ea4a-T8hoxG7PlYVDuNCXpFLeutVu7dykR4nELuZ8hF4OLZ1HVDVNPrK6P4K9jUyQH6C5AfNkMad4GU8hJe0Q__",
+              width: 200, // Set desired width
+              height: 200, // Set desired height
+             // fit: BoxFit.cover,
+            ),
+          ),
         ],
       ),
-
     );
   }
 }
